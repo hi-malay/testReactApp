b5e10f869afa0b5198c525d2e15e228ba1f9139d
Malay <hi.malay879@gmail.com>
2024-09-09 17:12:03 +0530

    fix: reverted fault code: ULTA_HPDP final QA sheet

--- src/components/clickTimeout.jsx
+++ src/components/clickTimeout.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./clickTimeout.css";
 
 export const ClickTimeout = () => {
   const [state, setState] = useState([]);
+  const intervalId = useRef(null);
   const changeColor = (data) => {
     setState((prevValue) => {
       return [...prevValue].includes(data)
@@ -10,27 +11,32 @@ export const ClickTimeout = () => {
         : [...prevValue, data];
     });
   };
+  useEffect(() => {
+    clearInterval(intervalId.current);
+  }, [state]);
 
   useEffect(() => {
     if (state.length === 7) {
-      const intervalId = setInterval(() => {
+      intervalId.current = setInterval(() => {
         setState((prevValue) => {
           prevValue.pop();
           return [...prevValue];
         });
 
         if (state.length === 0) {
-          clearInterval(intervalId); // Clear the interval if the array is empty
+          clearInterval(intervalId.current); // Clear the interval if the array is empty
           console.log("Array is empty. Stopping interval.");
         }
       }, 300);
     }
   }, [state]);
+
   return (
     <div className="container">
-      {[...Array(7).keys()].map((data, index) => {
+      {[...Array(7).keys()].map((data) => {
         return (
           <div
+            key={data}
             onClick={() => changeColor(data)}
             className={`box ${state.includes(data) ? "green" : ""}`}
           ></div>
